d4814c7138460a147d7e15dc01f5f0921fc9a95d
Saifuddin <adenwala32@gmail.com>
2024-04-10 10:01:43 +0530

    Floating action button added

--- app/src/main/java/com/myprojects/wishlistapp/ui/HomeView.kt
+++ app/src/main/java/com/myprojects/wishlistapp/ui/HomeView.kt
@@ -1,20 +1,23 @@
 package com.myprojects.wishlistapp.ui
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.IconButton
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -24,12 +27,24 @@ import androidx.compose.ui.unit.sp
 import com.myprojects.wishlistapp.R
 
 
+@SuppressLint("ResourceAsColor")
 @Preview(showBackground = true)
 @Composable
 fun HomeView() {
     Scaffold (
         modifier = Modifier,
-        topBar = { AppBar(title = "WishList", onBackNav = {}) }
+        topBar = { AppBar(title = "WishList", onBackNav = {}) },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(20.dp),
+                backgroundColor = Color(R.color.app_bar_color),
+                contentColor = Color.White,
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(corner = CornerSize(40.dp))
+            ) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Next screen navigation button")
+            }
+        }
     ) {
         LazyColumn(
             modifier = Modifier
@@ -41,7 +56,6 @@ fun HomeView() {
     }
 }
 
-//Just to make a commit
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBar(
@@ -49,15 +63,21 @@ fun AppBar(
     onBackNav: () -> Unit
 ) {
 
-    val navigationIcon: (@Composable () -> Unit)? = {
-        IconButton(onClick = onBackNav) {
-            Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                tint = Color.White,
-                contentDescription = null
-            )
+    val navigationIcon: (@Composable () -> Unit)? =
+        if(!title.contains("WishList")) {
+            {
+                IconButton(onClick = onBackNav) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        tint = Color.White,
+                        contentDescription = null
+                    )
+                }
+            }
+        }else {
+            null
         }
-    }
+
     TopAppBar(
         title = {
             Text(text = title,
@@ -69,7 +89,7 @@ fun AppBar(
             )
         },
         elevation = 3.dp,
-        backgroundColor = colorResource(id = R.color.app_bar_color), 
-        navigationIcon = { Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")}
+        backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
     )
 }
