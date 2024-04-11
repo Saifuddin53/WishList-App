76852f57c807d706581711eb8ae343ea358c210a
Saifuddin <adenwala32@gmail.com>
2024-04-12 05:14:56 +0530

    DAO created

--- app/src/main/java/com/myprojects/wishlistapp/data/WishDao.kt
+++ app/src/main/java/com/myprojects/wishlistapp/data/WishDao.kt
@@ -1,8 +1,26 @@
 package com.myprojects.wishlistapp.data
 
+import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
+@Dao
 abstract class WishDao {
     @Insert
-    abstract fun insertWish(wish: Wish)
+    abstract suspend fun insertAWish(wish: Wish)
+
+    @Query("Select * from `wish-table`")
+    abstract fun getAllWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract suspend fun updateAWish(wish: Wish)
+
+    @Delete
+    abstract suspend fun deleteAWish(wish: Wish)
+
+    @Query("Select * from `wish-table` where id = :id")
+    abstract fun getAWishById(id: Long): Flow<Wish>
 }
